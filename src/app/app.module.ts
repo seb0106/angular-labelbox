7c8be0a19521ce3740714512eacfd666abef6421
Sebastian Berchtold <sebastian.berchtold@sbb.ch>
2020-08-18 15:36:51 +0200

    DDD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ButtonModule} from '@sbb-esta/angular-public/button'
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,
+  ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
